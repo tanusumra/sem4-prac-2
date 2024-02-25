2aab217f0ec4927ccebcae3d0d171043d149a52b
tanusumra <141604389+tanusumra@users.noreply.github.com>
2024-02-25 08:29:57 +0500

    Add files via upload

---
+++ ArgCon.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Person
+{
+	Person(int a,int b)
+	{
+		System.out.println("Addition of the given two number is ::"+(a+b));
+	}
+}
+class ArgCon
+{
+	public static void main(String args[])
+	{
+		Person p1= new Person(10,20);
+	}
+}
